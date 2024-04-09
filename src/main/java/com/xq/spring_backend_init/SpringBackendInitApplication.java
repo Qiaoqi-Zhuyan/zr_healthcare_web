060de76ec158d76ac0ad087c7e6f1b0e36e5abe1
Qiaoqi-Zhuyan <shuxy6263@163.com>
2024-04-09 15:37:04 +0800

    updata .gitignore

---
+++ src/main/java/com/xq/spring_backend_init/SpringBackendInitApplication.java
@@ -0,0 +1,14 @@
+package com.xq.spring_backend_init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackendInitApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/xq/spring_backend_init/SpringBackendInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xq.spring_backend_init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
